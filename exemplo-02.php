daef3e72e3a718d090f47bec6241ad272734d8f6
Rogerio Black <rogeriodevelop@gmail.com>
2019-02-20 23:18:31 -0300

    Aulas de manipulação de Strings

---
+++ exemplo-02.php
@@ -0,0 +1,29 @@
+<?php
+
+	$nome = "rogerio santos";
+
+	echo $nome;
+	
+	echo "<br>";
+
+	$nome = strtoupper($nome);
+
+	echo $nome;
+
+	echo "<br>";
+
+	$nome = strtolower($nome);
+
+	echo $nome;
+
+	echo "<br>";
+
+	//echo ucwords($nome);
+
+	echo ucfirst($nome);
+
+	echo "<br>";
+
+
+
+
